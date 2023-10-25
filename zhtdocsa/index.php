4571456ca4d5a17a11c0f5a78133c86fa5510d5a
kl0cek <118082405+kl0cek@users.noreply.github.com>
2023-10-25 23:05:41 +0200

    jakis start ni

---
+++ zhtdocsa/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta chatset="UTF-8" >
+        <title>SYSTEM KINOWY</title>
+        <link rel="stylesheet" href="style.css">
+        <meta name="theme-color" content="#808080">
+    </head>
+    <body>
+    <script type="text/javascript" src="skrypt.js">   
+    </script>
+    <nav>
+        <h2> NAWIGACJA </h2>    
+        
+            <!--<li><a href="KUPBILET.php" target="_blank"> KUPBILET </a></li> -->
+            <a href="MOJEBILETY.php" target=""> MOJEBILETY</a>
+        
+    </nav>
+    <header>
+        <h1>SYSTEM OBSLUGI KINA</h1>
+    </header>
+    <main>
+        tutaj repertuar itd obrazek moze no i tytul godizny 
+        imo mozna zrobic ze po kliknieciu na strone rezerwacji ale idk
+    </main>
+    <footer>
+        <p>patrzysz mi sie na bebech?</p>
+    </footer>
+    </body>
+</html>
+<?php 
+//deklaracja funkcji bo ladniej wyglada
+//wywoluje sie <?php funkcja();? >
+/*
+$conn = mysqli_connect("localhost", "root", "", "nazwabazy");
+    if(!$conn){
+        echo "nie udalo sie polaczyc z baza";
+        return;
+    }else{
+
+    }
+
+
+*/
+?>
