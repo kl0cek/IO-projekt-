00cd0b72c8f0b4b6268d2ad55a9531ad818fd336
JakubLatawiec <106024697+JakubLatawiec@users.noreply.github.com>
2023-10-29 11:36:03 +0100

    API dodane

---
+++ API/api.js
@@ -0,0 +1,55 @@
+var config = require('./dbconfig');
+const sql = require('mssql');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+var router = express.Router();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', router);
+
+router.use((request, response, next) => {
+    console.log('middleware');
+    next();
+});
+
+//Get movies
+router.route('/movies').get(async (request, response) => {
+  try {
+    let pool = await sql.connect(config);
+    let data = await pool.request().execute('GetMovies');
+    response.status(200);
+    response.json(data.recordset);
+  } catch (error) {
+    response.status(500).json(error);
+  }
+})
+
+//Get movie by ID
+router.route('/movies/:id').get(async (request, response) => {
+  let pool = await sql.connect(config);
+  let data = await pool.request().input('id', request.params['id']).execute('GetMoviesByID');
+  response.json(data.recordset);
+})
+
+//Get movie screening
+router.route('/screening/:id').get(async (request, response) => {
+  let pool = await sql.connect(config);
+  let data = await pool.request().input('movieID', request.params['id']).execute('GetMovieScreening');
+  response.json(data.recordset);
+})
+
+router.route('/test/:id').get(async (request, response) => {
+  let pool = await sql.connect(config);
+  let data = await pool.request().input('MovieID', request.params['id']).execute('test');
+  response.json(data.recordsets);
+  console.log(data);
+})
+
+
+var port = process.env.PORT || 8090;
+app.listen(port);
+console.log('Order API is runnning at ' + port);
